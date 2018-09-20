2ed82b57e4dbb0ee3e25d8cfd21fad6954cc2b86
chhantea <r.lalnghenchhana@gmail.com>
2018-09-20 16:11:10 +0530

    continue

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match ':controller(/:action(/:id))', :via => [:get, :post]
 end
